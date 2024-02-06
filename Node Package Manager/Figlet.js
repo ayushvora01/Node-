0d081d3c5ebc8e976ab3264184253dfb33e6dbb3
ayushvora01 <128839365+ayushvora01@users.noreply.github.com>
2024-02-06 19:10:33 +0530

    Understanding about NPM with Figlet

---
+++ Node Package Manager/Figlet.js
@@ -0,0 +1,10 @@
+const Figlet = require('figlet');
+
+Figlet("Ayush Vora", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
